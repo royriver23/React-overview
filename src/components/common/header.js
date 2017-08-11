d5c28ddc8882024e588757373873abb677848355
royriver23 <roy.fita23@gmail.com>
2017-08-10 22:02:19 -0600

    master: Adding logos

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -8,7 +8,7 @@ var Header = React.createClass({
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <a href="/" className="navbar-brand">
-            <img src="images/logo.png" />
+            <img src="images/logo.ico" />
           </a>
           <ul className="nav navbar-nav">
             <li><a href="/">Home</a></li>
